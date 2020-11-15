25e721e13de65ca7f3a4820df5177bb7b5628649
LasithEranga <73494729+LasithEranga@users.noreply.github.com>
2020-11-15 13:55:25 +0530

    ContactList.java file Created

---
+++ ContactList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.io.File;
+
+public class ContactList{
+    public ArrayList<String> ContactList = new ArrayList<String>();
+    
+    void addContact(){
+        //Dilumi
+    }
+    
+    void removeContact(){
+        //Supun
+    }
+    
+    void editContact(){
+        //Lasith
+    }
+    void displayContacts(){
+        //
+    }
+    void  loadContacts(){
+        //loads from a file
+    }
+    void saveContacts(){
+           //save to a file
+    }
+
+}
