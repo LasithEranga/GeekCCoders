85987a22a9086379ffa68b632b8b61da97f9bd19
LasithEranga <73494729+LasithEranga@users.noreply.github.com>
2020-11-15 14:24:45 +0530

    Added edit contact Method

---
+++ ContactList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.io.File;
+
+public class ContactList{
+    public HashMap<String,String> ContactList = new HashMap<String,String>();
+    
+    void addContact(){
+        //Dilumi
+    }
+    
+    void removeContact(){
+        //Supun
+    }
+    
+    void editContact(){
+        //Lasith
+        ContactList.get("Strin name");
+    }
+    void displayContacts(){
+        //
+    }
+    void  loadContacts(){
+        //loads from a file
+    }
+    void saveContacts(){
+           //save to a file
+    }
+
+}
