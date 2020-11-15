34edd9136cc6272db76a4279b670a8d7c18123be
Supun222 <74459467+Supun222@users.noreply.github.com>
2020-11-15 17:26:54 +0530

    ContactList.java

--- ContactList.java
+++ ContactList.java
@@ -1,112 +1,94 @@
+
 import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Map;
 
 
-public class ContactList{
-    public HashMap<String,String> ContactList = new HashMap<String,String>();
+public class ContactList {
 
+    
+    private HashMap<String,String> ContactMap = new HashMap<String,String>();
+    
+    Contact contact = new Contact();
+    
     Scanner sc = new Scanner(System.in);
     
-    void addContact(){
-        //Dilumi
+    void removeContact(HashMap <String,String> ContactMap){
+        try{
+        System.out.print("Please enter name of the contact that you want to delete:");
+        contact.setName(sc.nextLine());
+        ContactMap.remove(contact.getName(),ContactMap.get(contact.getName()));
+        }
+        catch (Exception e){
+            System.out.println("Name does not exist!");
+        }
     }
     
-    void removeContact(String name, String phonenumber){
-        
-        
-        for(int i=0; i<ContactList.size; i++){
-            
-            if (name == ContactList.key ){
-                
-                String ask = null;
-                while(ask == "1"){
-                System.out.println("\n-------------------------------" );
-                System.out.println("Name : " + ContactList.key );
-                System.out.println("Phone number : " + ContactList.value );
-                System.out.println("-------------------------------" );
-                System.out.println("\nDo you want remove contact ?\n 1.Yes\t 2.No" + ContactList.key );
-                
-                ask = sc.nextLine();
-                
-                if(ask == "1"){
-                    ContactList.key.clear();
-                    ContactList.value.clear();
-                    System.out.println("\n-------------------------------" );
-                    System.out.println("Conatct is deleted succesfully" + ContactList.key );
-                    System.out.println("--------------------------------"  );
-                    break;
-                }
-                else if(ask=="2"){
-                    System.out.println("\n-------------------------------" );
-                    System.out.println("Name : " + ContactList.key );
-                    System.out.println("Phone number : " + ContactList.value );
-                    System.out.println("-------------------------------" );
-                    System.out.println("\nDo you want remove contact ?\n 1.Yes\t 2.No" + ContactList.key );
-                
-                    ask = sc.nextLine();
-                }
-                else{
-                    System.out.println("Invalid input please try" );
-                }
-                }
-            }
-            else{
-                
-                System.out.println("Cannot find a name " + ContactList.key);
-                
-            }
-            
-            
-            
-        }
-        
-    
-    void addContact(){
+    void addContact(HashMap <String,String> ContactMap){
         //Dilumi
         System.out.print("PLease enter name: ");
-        String name = sc.nextLine();
-        System.out.println("Please add phone number: ");
-        String phone = sc.nextLine();
-	      ContactList.put(name,phone);
+        contact.setName(sc.nextLine());
+        System.out.print("Please add phone number: ");
+        contact.setPhoneNo(sc.nextLine());
+	ContactMap.put(contact.getName(),contact.getPhoneNo());
+        
     }
     
-    void removeContact(){
-        //Supun
+
+    void displayContacts(HashMap <String,String> ContactMap){
+       System.out.printf("Name\t\tNumber\n\n");
+       for (String i : ContactMap.keySet()) {
+	  System.out.printf("%s\t\t%s\n" , i, ContactMap.get(i));
+	}
     }
-    
-    void editContact(){
+
+    void editContact(HashMap <String,String> ContactMap){
         //Lasith
-
-        ContactList.get("Strin name");
-    }
-    void displayContacts(){
-        //
-
-
-        Scanner sc = new Scanner(System.in);
         System.out.print("Please enter name of the contact that you want to change:");
-        String name = sc.nextLine();
+        contact.setName(sc.nextLine());
         System.out.print("Please enter new  phone number of the contact that you want to change:");
-        String number = sc.nextLine();
-        ContactList.replace(name, number);
+        contact.setPhoneNo(sc.nextLine());
+        ContactMap.replace(contact.getName(),contact.getPhoneNo());
     }
-    void displayContacts(){
-        //
-        ContactList.get("Strin name");
-    }
-    void displayContacts(){
 
-        //
-
-        //PK
-    }
-    void  loadContacts(){
+ 
+    void  loadContacts(HashMap <String,String> ContactMap){
         //loads from a file
+        try {
+            File ContactFile = new File("ContactFile.txt");
+            Scanner contact = new Scanner(ContactFile);
+	    System.out.printf("Name\t\tNumber\n\n");
+            while (contact.hasNextLine()) {
+            String data = contact.nextLine();
+            String[] Ndata = data.split(" ");
+            ContactMap.put(Ndata[0],Ndata[1]);
+           // System.out.println(data);
+            System.out.printf("%s\t\t%s\n" , Ndata[0], Ndata[1]);
+      }
+      contact.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-    void saveContacts(){
-           //save to a file
     }
 
+    void saveContacts(HashMap <String,String> ContactMap){
+            try {
+                FileWriter SaveTo = new FileWriter("ContactFile.txt");
+                for (String i : ContactMap.keySet()) {
+                    SaveTo.write(i + " " + ContactMap.get(i)+'\n');
+                }       
+            SaveTo.close();
+                System.out.println("Successfully wrote to the file.");
+            } catch (IOException e) {
+                 System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+    }
+    
 }
